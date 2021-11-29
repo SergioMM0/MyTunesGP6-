342be81a3bf1957f82237678ef30f062ee2b255b
Sergio M <serg0971@easv365.dk>
2021-11-29 12:04:03 +0100

    .

---
+++ src/main/Main.java
@@ -0,0 +1,28 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    /*
+Initialises main window
+ */
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("gui/view/MainView.fxml"));
+        Scene scene = new Scene(root, 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ src/main/be/Song.java
@@ -0,0 +1,7 @@
+package be;
+
+public class Song {
+    public void hi(){
+        System.out.println("hi");
+    }
+}
