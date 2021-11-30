728a69c6521723932357846914f849f22db685bb
Sergio M <serg0971@easv365.dk>
2021-11-30 19:24:08 +0100

    .

---
+++ src/main/dal/MyTunesDalFacade.java
@@ -0,0 +1,32 @@
+package dal;
+
+import be.Playlist;
+import be.Song;
+
+import java.util.List;
+
+public interface MyTunesDalFacade {
+
+    Song createSong(int id, String name, String artist, String category, int duration);
+
+    List<Song> getAllSongs();
+
+    void deleteSong(Song song);
+
+    void updateSong(Song song);
+
+    Song getSong(int id);
+
+    //******
+
+    Playlist createPlaylist();//TODO implement parameters
+
+    List<Playlist> getAllPlaylist();
+
+    void deletePlaylist(Playlist playlist);
+
+    void updatePlaylist(Playlist playlist);
+
+    Playlist getPlaylist(int id);
+
+}
