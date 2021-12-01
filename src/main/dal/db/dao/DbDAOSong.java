a8a3f535c7d3f5f5c3d7d756863548ca77e441ef
Sergio M <serg0971@easv365.dk>
2021-12-01 17:02:01 +0100

    DbDao impl

--- src/main/dal/db/dao/DbDAOSong.java
+++ src/main/dal/db/dao/DbDAOSong.java
@@ -1,21 +1,53 @@
 package dal.db.dao;
 
 import be.Song;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import dal.db.DBConnectionProvider;
 import dal.interfaces.ISongRepository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class DbDAOSong implements ISongRepository {
 
-
+    private final DBConnectionProvider connectionProvider = new DBConnectionProvider();
 
     @Override
-    public Song addSong(int id, String name, String artist, String category, int duration) {
-        return null;
+    public Song addSong(int id, String name, String artist, String category, int duration,String filePath) throws SQLException {
+        String sql = ("INSERT INTO song VALUES (?,?,?,?,?,?)");
+        Song addedSong = null;
+        try (Connection connection = connectionProvider.getConnection()) {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setInt(1, id);
+            st.setString(2, name);
+            st.setString(3, artist);
+            st.setString(4, category);
+            st.setInt(5, duration);
+            st.setString(6, filePath);
+            boolean execute = st.execute();
+            if (!execute) {
+                addedSong = new Song(id, name, artist, category, duration, filePath);
+            }
+        } catch (SQLServerException throwables) {
+            throwables.printStackTrace();
+        }
+        return addedSong;
     }
 
     @Override
-    public void deleteSong(Song song) {
+    public void deleteSong(Song song) throws SQLServerException {
+        String sql = ("DELETE FROM Song WHERE id = ?");
+
+
+        try (Connection connection = connectionProvider.getConnection()){
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setInt(1,);
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
 
     }
 
--- src/main/dal/interfaces/ISongRepository.java
+++ src/main/dal/interfaces/ISongRepository.java
@@ -1,14 +1,16 @@
 package dal.interfaces;
 
 import be.Song;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface ISongRepository {
 
-    Song addSong(int id, String name, String artist, String category, int duration);
+    Song addSong(int id, String name, String artist, String category, int duration,String filePath)throws SQLException;
 
-    void deleteSong(Song song);
+    void deleteSong(Song song) throws SQLServerException;
 
     void updateSong(Song song);
 
