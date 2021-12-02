11a2acfb3c3f130e000bc2e5cd13e593fe60eeb5
DanniGisla <91472044+DanniGisla@users.noreply.github.com>
2021-12-02 13:39:37 +0100

    .

--- src/main/gui/controller/MainViewController.java
+++ src/main/gui/controller/MainViewController.java
@@ -2,9 +2,15 @@ package gui.controller;
 
 import gui.model.SongModel;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
-public class MainViewController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainViewController implements Initializable {
     @FXML
     private Label welcomeText;
 
@@ -12,8 +18,16 @@ public class MainViewController {
 
     @FXML
     protected void onHelloButtonClick() {
+
+
         songModel.getAllSongs();
     }
 
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Media media = new Media("/Users/daniel/Documents/GitHub/MyTunesGP6-/Michael.mp3");
+        MediaPlayer mp = new MediaPlayer(media);
+        mp.play();
+    }
 }
