ae5f855242c12e7d4ca51c06172f7164202a11a7
DanniGisla <91472044+DanniGisla@users.noreply.github.com>
2021-12-02 15:32:51 +0100

    .

--- src/main/gui/controller/MainViewController.java
+++ src/main/gui/controller/MainViewController.java
@@ -15,5 +15,79 @@ public class MainViewController {
         songModel.getAllSongs();
     }
 
+    @FXML
+    protected void playSong() {
+
+    }
+
+    @FXML
+    protected void nextSong() {
+
+    }
+    @FXML
+    protected void previousSong() {
+
+    }
+
+    @FXML
+    protected void changeVolume() {
+
+    }
+
+    @FXML
+    protected void showResult() {
+
+    }
+
+    @FXML
+    protected void addSong() {
+
+    }
+
+    @FXML
+    protected void newPlaylist() {
+
+    }
+
+    @FXML
+    protected void editPlaylist() {
+
+    }
+
+    @FXML
+    protected void deletePlaylist() {
+
+    }
+
+    @FXML
+    protected void moveUp() {
+
+    }
+
+    @FXML
+    protected void moveDown() {
+
+    }
+
+    @FXML
+    protected void removeSong() {
+
+    }
+
+    @FXML
+    protected void editSong() {
+
+    }
+
+    @FXML
+    protected void newSong() {
+
+    }
+
+    @FXML
+    protected void deleteSong() {
+
+    }
+
 
 }
