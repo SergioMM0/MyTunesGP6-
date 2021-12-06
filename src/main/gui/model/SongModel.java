4e30fd7ea8e84ccb22378c1d782ccdaf0444063c
Sergio M <serg0971@easv365.dk>
2021-12-06 13:26:08 +0100

    New Main View

--- src/main/gui/model/SongModel.java
+++ src/main/gui/model/SongModel.java
@@ -1,55 +1,5 @@
 package gui.model;
 
-import be.Song;
-import javafx.collections.ObservableList;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class SongModel {
-    // Static variable reference of single_instance
-    // of type Singleton
-    private static SongModel single_instance = null;
 
-    // Declaring a variable of type String
-    private List<Song> cachedSongs;
-
-    // Constructor
-    // Here we will be creating private constructor
-    // restricted to this class itself
-    private SongModel()
-    {
-        cachedSongs = new ArrayList<>();
-        //cahcedSongs = bllInterface.getAllSongs;
-    }
-
-    // Static method
-    // Static method to create instance of Singleton class
-    public static SongModel getInstance()
-    {
-        if (single_instance == null)
-            single_instance = new SongModel();
-
-        return single_instance;
-    }
-
-    public List<Song> getAllSongs(){
-        return cachedSongs;
-    }
-
-    public void playSong(ObservableList<Song> selectedItems) {
-
-    }
-
-    public void previousSong(Song song) {
-
-    }
-
-    public void nextSong(Song song) {
-
-    }
-
-    public ObservableList<Song> getResult(String text) {
-        return null;
-    }
 }
