27d34d33279f70cfc05dd48c8f460e27250c0bcf
MokiesB <90444262+MokiesB@users.noreply.github.com>
2021-12-06 13:55:39 +0100

    .

--- src/main/gui/controller/NewEditPlaylist.java
+++ src/main/gui/controller/NewEditPlaylist.java
@@ -1,9 +1,16 @@
 package gui.controller;
 
+import be.Playlist;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 
 public class NewEditPlaylist {
+    public TextField playlistname;
+    public Button cancel1btn;
+    public Button save1btn;
     private MainViewController controller;
 
     public void closeNEPWindow(MouseEvent mouseEvent) {
@@ -13,7 +20,11 @@ public class NewEditPlaylist {
     public void newPlaylist(MouseEvent mouseEvent) {
 
     }
+
     public void setParentController(MainViewController controller){
         this.controller = controller;
+    }
+
+    public void setPlaylistName(ObservableList<Playlist> selectedItems) {playlistname.setText(String.valueOf(selectedItems));
     }
 }
